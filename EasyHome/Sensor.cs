60a52ea6a7efd0daca2e7848547817d353449a23
Zsolt Filarszky <zsolt.filarszky@gmail.com>
2016-11-29 16:25:15 +0100

    Init

---
+++ EasyHome/Sensor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace api_tester
+{
+    public class Sensor
+    {
+        [JsonProperty(PropertyName = "id")]
+        public int Id { get; set; }
+
+        [JsonProperty(PropertyName = "type")]
+        public string Type { get; set; }
+
+        [JsonProperty(PropertyName = "changeable")]
+        public Changable Changeable { get; set; }
+
+        public Sensor(int id, string type, Changable changeable)
+        {
+            Id = id;
+            Type = type;
+            Changeable = changeable;
+        }
+    }
+
+
+}
